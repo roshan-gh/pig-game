d7a26af6372c89d2a95de48054baf7ca7dc8a310
Roshan Ghojoghi <roshanghojoghi@gmail.com>
2022-04-09 04:24:47 +0300

    fixed some bugs

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let player2Score = document.getElementById('score--1');
 let dice = document.querySelector('.dice');
 let player1CurrentScoreDisplay = document.getElementById('current--0');
 let player2CurrentScoreDisplay = document.getElementById('current--1');
-let newGameButton = document.querySelector('.btn--new');
+let btnRoll = document.querySelector('.btn--roll');
+let btnHold = document.querySelector('.btn--hold');
+let btnNew = document.querySelector('.btn--new');
 // Just game starts
 let player1 = document.querySelector('.player--0');
 let player2 = document.querySelector('.player--1');
@@ -13,7 +15,7 @@ player2Score.textContent = 0;
 let player1CurrentScore = 0;
 let player2CurrentScore = 0;
 dice.classList.add('hidden');
-newGameButton.classList.add('hidden');
+btnNew.classList.add('hidden');
 let togglePlayer = true;
 let choosePlayer = true;
 let player1Win = false;
@@ -21,7 +23,7 @@ let player1Win = false;
 // Shows the dice with a random score
 const rollDice = function() {
     const randomDiceNumber = Math.floor(Math.random() * 6) + 1;
-    document.querySelector('img').setAttribute('src', `images/dice-${randomDiceNumber}.png`);
+    dice.src = `images/dice-${randomDiceNumber}.png`;
     dice.classList.remove('hidden');
     return randomDiceNumber;
 }
@@ -47,12 +49,11 @@ const switchPlayer = function() {
 
 // Main game logic
 const gameLogic = function() {
-    let score = rollDice();
+    const score = rollDice();
 
     if (score !== 1 && togglePlayer === true) {
         player1CurrentScore += score;
         player1CurrentScoreDisplay.textContent = String(player1CurrentScore);
-
     } else if (score !== 1 && togglePlayer === false) {
         player2CurrentScore += score;
         player2CurrentScoreDisplay.textContent = String(player2CurrentScore);
@@ -71,7 +72,9 @@ const winGame = function(win) {
         player2.classList.add('player--winner');
         player1.classList.remove('player--active');
     }
-    newGameButton.classList.remove('hidden');
+    btnNew.classList.remove('hidden');
+    btnHold.disabled = true;
+    btnRoll.disabled = true;
 }
 
 // Sums the current score to scores
@@ -101,7 +104,7 @@ const newGame = function() {
     let player1CurrentScore = 0;
     let player2CurrentScore = 0;
     dice.classList.add('hidden');
-    newGameButton.classList.add('hidden');
+    btnNew.classList.add('hidden');
     let togglePlayer = true;
     let choosePlayer = true;
     let player1Win = false;
@@ -109,14 +112,15 @@ const newGame = function() {
     player2.classList.remove('player--winner');
     player2.classList.remove('player--active');
     player1.classList.add('player--active');
-
+    btnHold.disabled = false;
+    btnRoll.disabled = false;
 }
 
 //Click event on the roll the dice button
-document.querySelector('.btn--roll').addEventListener('click', gameLogic);
+btnRoll.addEventListener('click', gameLogic);
 
 //Click event on the hold button
-document.querySelector('.btn--hold').addEventListener('click', holdScore);
+btnHold.addEventListener('click', holdScore);
 
 //Click event on the New Game button
-document.querySelector('.btn--new').addEventListener('click', newGame);
+btnNew.addEventListener('click', newGame);
